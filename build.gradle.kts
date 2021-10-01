61db36567c43b6252efdc3ac310a09b9efd0dc46
jyunkim <jihyun.kim@bucketplace.net>
2021-10-01 15:21:23 +0900

    style: add footnotes

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     id("org.springframework.boot") version "2.5.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.5.31"
-    kotlin("plugin.spring") version "1.5.31"
-    kotlin("plugin.jpa") version "1.5.31"
+    kotlin("plugin.spring") version "1.5.31" // allopen을 포함하지만 엔티티에는 적용되지 않음
+    kotlin("plugin.jpa") version "1.5.31" // noarg를 포함
     kotlin("plugin.allopen") version "1.5.31"
 }
 
