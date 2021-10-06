86e5620a235e372bfdc31c74e82871fff261f64e
jyunkim <jihyun.kim@bucketplace.net>
2021-10-06 18:25:29 +0900

    fix: fix parameter order

--- src/test/kotlin/ohouse/jpakotlin/JpaKotlinApplicationTests.kt
+++ src/test/kotlin/ohouse/jpakotlin/JpaKotlinApplicationTests.kt
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.Commit
 import org.springframework.transaction.annotation.Transactional
-import java.time.LocalDateTime
 import javax.persistence.EntityManager
 
 @SpringBootTest
@@ -28,7 +27,7 @@ class JpaKotlinApplicationTests {
         val option = Option(0, "asdf")
         em.persist(option)
 
-        val orderOption = OrderOption(0, order, option, "asdf", 100.0, 10, 1000.0)
+        val orderOption = OrderOption(0, order, option, "asdf", 10, 100.0, 1000.0)
         em.persist(orderOption)
 
         val payment = Payment(0, orderOption, PaymentMethod.CARD, 100)
