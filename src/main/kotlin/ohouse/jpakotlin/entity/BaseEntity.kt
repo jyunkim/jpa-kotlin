9dae7c6b18dfbe403a0ad46be1393577be2cf600
JyunKim <kann1997@naver.com>
2021-10-07 00:10:43 +0900

    refactor: add pre event methods and change setter to protected

--- src/main/kotlin/ohouse/jpakotlin/entity/BaseEntity.kt
+++ src/main/kotlin/ohouse/jpakotlin/entity/BaseEntity.kt
@@ -1,11 +1,30 @@
 package ohouse.jpakotlin.entity
 
 import java.time.LocalDateTime
+import javax.persistence.Column
 import javax.persistence.MappedSuperclass
+import javax.persistence.PrePersist
+import javax.persistence.PreUpdate
 
 @MappedSuperclass
 abstract class BaseEntity {
+
+    @Column(updatable = false)
     var createdAt: LocalDateTime? = null
+        protected set // open 프로퍼티는 private setter를 가질 수 없음
     var updatedAt: LocalDateTime? = null
+        protected set
     var isDeleted: Boolean = false
+
+    @PrePersist
+    fun prePersist() {
+        val now = LocalDateTime.now()
+        createdAt = now
+        updatedAt = now
+    }
+
+    @PreUpdate
+    fun preUpdate() {
+        updatedAt = LocalDateTime.now()
+    }
 }
