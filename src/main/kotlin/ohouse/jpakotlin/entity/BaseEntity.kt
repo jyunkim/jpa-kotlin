6c94415dbe44df5ddfccea9349e31f13eed3cfee
JyunKim <kann1997@naver.com>
2021-10-07 01:36:14 +0900

    feat: add remove method

--- src/main/kotlin/ohouse/jpakotlin/entity/BaseEntity.kt
+++ src/main/kotlin/ohouse/jpakotlin/entity/BaseEntity.kt
@@ -14,10 +14,12 @@ abstract class BaseEntity {
         protected set // open 프로퍼티는 private setter를 가질 수 없음
     var updatedAt: LocalDateTime? = null
         protected set
-    var isDeleted: Boolean = false
+    var isRemoved: Boolean = false
+        protected set
 
     @PrePersist
     fun prePersist() {
+        // 따로 초기화하면 createdAt과 updatedAt에 차이가 생김
         val now = LocalDateTime.now()
         createdAt = now
         updatedAt = now
@@ -26,5 +28,9 @@ abstract class BaseEntity {
     @PreUpdate
     fun preUpdate() {
         updatedAt = LocalDateTime.now()
+    }
+
+    fun remove() {
+        isRemoved = true
     }
 }
