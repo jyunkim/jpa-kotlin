657ec806f2b421c8657a18c843381345cb7ed9ce
jyunkim <jihyun.kim@bucketplace.net>
2021-10-07 12:13:58 +0900

    style: add footnotes

--- src/main/kotlin/ohouse/jpakotlin/entity/Member.kt
+++ src/main/kotlin/ohouse/jpakotlin/entity/Member.kt
@@ -12,12 +12,14 @@ class Member private constructor(
     age: Int
 ) : BaseEntity() {
 
+    // Id는 생성자로 받을 필요가 없으므로 빼줌
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null // Id는 save 시 값을 넣어주지 않으므로 nullable 해야 함
 
     // 탈퇴나 휴면 전환 시 null로 세팅(name으로 조회 시 나오지 않도록)
     // 더이상 사용하지 않아도 DB에 남겨놔야 하기 때문에 null 허용해서 처리
+    // var 프로퍼티는 setter를 protected로 설정하기 위해 필드에서 선언
     var name: String? = name
         protected set
     var age: Int = age
