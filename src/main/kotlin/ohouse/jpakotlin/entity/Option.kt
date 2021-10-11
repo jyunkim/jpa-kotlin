11a1794d2e0dce67366a65e37ab7a6fb7a998aa9
jyunkim <jihyun.kim@bucketplace.net>
2021-10-12 01:06:48 +0900

    refactor: add product and stock quantity properties

--- src/main/kotlin/ohouse/jpakotlin/entity/Option.kt
+++ src/main/kotlin/ohouse/jpakotlin/entity/Option.kt
@@ -5,15 +5,33 @@ import javax.persistence.*
 @Entity
 @Table(name = "options")
 class Option private constructor(
-    val optionName: String
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    val product: Product,
+
+    val name: String,
+    val price: Int,
+    stockQuantity: Int
 ) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
 
+    var stockQuantity: Int = stockQuantity
+        protected set
+
     companion object {
-        fun of(optionName: String) =
-            Option(optionName)
+        fun of(product: Product, name: String, price: Int, stockQuantity: Int) =
+            Option(product, name, price, stockQuantity)
+    }
+
+    fun addStock(quantity: Int) {
+        stockQuantity += quantity
+    }
+
+    fun removeStock(quantity: Int) {
+        if (stockQuantity < quantity) throw IllegalArgumentException("재고가 부족합니다.")
+        stockQuantity -= quantity
     }
 }
