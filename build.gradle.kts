25c05cc0750adbe9f9f81ae2854df2f47048406f
jyunkim <jihyun.kim@bucketplace.net>
2021-10-13 17:46:04 +0900

    chore: add querydsl settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,14 @@ plugins {
     kotlin("plugin.spring") version "1.5.31" // allopen을 포함하지만 엔티티에는 적용되지 않음
     kotlin("plugin.jpa") version "1.5.31" // noarg를 포함 -> 기본 생성자가 public으로 생성되지만 리플렉션으로만 사용 가능
     kotlin("plugin.allopen") version "1.5.31"
+    kotlin("kapt") version "1.5.31" // 코틀린 코드의 annotation process를 지원
+    idea
 }
 
 group = "ohouse"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
+val querydslVersion = "4.4.0"
 
 repositories {
     mavenCentral()
@@ -22,6 +25,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Querydsl
+    implementation("com.querydsl:querydsl-jpa:${querydslVersion}")
+    kapt("com.querydsl:querydsl-apt:${querydslVersion}:jpa")
+
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
@@ -35,6 +43,15 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+idea {
+    // Querydsl
+    module {
+        val kaptMain = file("build/generated/source/kapt/main")
+        sourceDirs.add(kaptMain)
+        generatedSourceDirs.add(kaptMain)
+    }
 }
 
 allOpen {
