f500b64ccfe41605c3035b86c5825fe79d690629
JyunKim <kann1997@naver.com>
2021-10-15 01:24:32 +0900

    feat: add member service

--- src/main/kotlin/ohouse/jpakotlin/service/MemberService.kt
+++ src/main/kotlin/ohouse/jpakotlin/service/MemberService.kt
@@ -1,6 +1,6 @@
 package ohouse.jpakotlin.service
 
-import ohouse.jpakotlin.entity.Member
+import ohouse.jpakotlin.dto.MemberDto
 import ohouse.jpakotlin.repository.MemberRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
@@ -12,23 +12,38 @@ class MemberService(
     private val memberRepository: MemberRepository // private을 붙이면 open -> final됨
 ) {
 
+    fun getProfile(id: Long): MemberDto.Response {
+        val findMember = findMember(id)
+        return MemberDto.Response.of(findMember)
+    }
+
     @Transactional
-    fun signup(member: Member): Long {
+    fun signUp(member: MemberDto.Request): MemberDto.Result {
         validate(member)
-        memberRepository.save(member)
-        return member.id!!
+        memberRepository.save(member.toEntity())
+        return MemberDto.Result(true, "회원가입이 완료되었습니다.")
     }
 
-    fun findMembers(): List<Member> {
-        return memberRepository.findAll()
+    @Transactional
+    fun updateInfo(id: Long, member: MemberDto.Request): MemberDto.Result {
+        val findMember = findMember(id)
+        findMember.updateInfo(member.toEntity())
+        return MemberDto.Result(true, "정보 수정이 완료되었습니다.")
     }
 
-//    fun findMember(id: Long): Member {
-//        return memberRepository.findByIdOrNull(id)
-//    }
+    @Transactional
+    fun deleteAccount(id: Long): MemberDto.Result {
+        val findMember = findMember(id)
+        findMember.remove()
+        return MemberDto.Result(true, "회원 탈퇴가 완료되었습니다.")
+    }
+
+    private fun findMember(id: Long) =
+        memberRepository.findByIdOrNull(id) ?: throw IllegalStateException("회원이 존재하지 않습니다.")
 
     // 중복 회원 검증
-    private fun validate(member: Member) {
-        memberRepository.findByEmail(member.email).let { throw IllegalStateException("이미 존재하는 회원입니다.") }
+    private fun validate(member: MemberDto.Request) {
+        val findMember = memberRepository.findByEmail(member.email)
+        findMember?.let { throw IllegalStateException("이미 존재하는 회원입니다.") }
     }
 }
