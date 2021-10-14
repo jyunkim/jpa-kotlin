9c30dd4d9d09793e19015fc0a18ce8f32e1e4463
JyunKim <kann1997@naver.com>
2021-10-15 01:24:24 +0900

    feat: add member dto

--- src/main/kotlin/ohouse/jpakotlin/dto/MemberDto.kt
+++ src/main/kotlin/ohouse/jpakotlin/dto/MemberDto.kt
@@ -1,12 +1,49 @@
 package ohouse.jpakotlin.dto
 
+import ohouse.jpakotlin.entity.Member
+
 object MemberDto {
 
-    object Request {
+    // post 요청 시 ObjectMapper를 사용하기 때문에 setter 필요x
+    data class Request(
+        val email: String,
+        val password: String,
 
+        val name: String,
+        val age: Int,
+        val phoneNumber: String,
+        val address: String?
+    ) {
+
+        fun toEntity(): Member =
+            Member.of(email, password, name, age, phoneNumber, address)
     }
 
-    object Response {
+    data class Response(
+        val email: String,
+        val password: String,
 
+        val name: String?,
+        val age: Int,
+        val phoneNumber: String,
+        val address: String?
+    ) {
+
+        companion object {
+            fun of(member: Member) =
+                Response(
+                    member.email,
+                    member.password,
+                    member.name,
+                    member.age,
+                    member.phoneNumber,
+                    member.address
+                )
+        }
     }
+
+    data class Result(
+        val success: Boolean,
+        val message: String?
+    )
 }
