bcae28bc30f8032992c4fd25ddb0bb7dd67c0ab5
jyunkim <jihyun.kim@bucketplace.net>
2021-10-15 19:11:45 +0900

    feat: add kotest extensions spring config

---
+++ src/test/kotlin/ohouse/jpakotlin/ProjectConfig.kt
@@ -0,0 +1,8 @@
+package ohouse.jpakotlin
+
+import io.kotest.core.config.AbstractProjectConfig
+import io.kotest.extensions.spring.SpringExtension
+
+class ProjectConfig : AbstractProjectConfig() {
+    override fun extensions() = listOf(SpringExtension)
+}
